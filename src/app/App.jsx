4bf2d036f39d8c93739d77709e04d6bf454b9e71
Leonid <leosagir1983@gmail.com>
2024-07-07 19:34:19 +0200

    test

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css';
-import Header from './components/Header/Header';
-import LoginForm from './components/LoginForm/loginForm';
+import Header from '../components/Header/Header';
+import LoginForm from '../components/LoginForm/loginForm';
 
 
 function App() {
